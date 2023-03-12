edcf24c38a5e5787f336f18310b713d919b78859
JoshuaMaher <jmaher300@caledonian.ac.uk>
2023-03-12 21:40:45 +0000

    Enemy specified tooth

--- Assets/Scripts/DENTOID/Enemy/EnemyDamage.cs
+++ Assets/Scripts/DENTOID/Enemy/EnemyDamage.cs
@@ -5,20 +5,29 @@ public class EnemyDamage : MonoBehaviour
 {
     [SerializeField] protected float damage;
 
+    private bool hasCollided = false;
+
+   
+
+    
+
 
     protected void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Molar")
+        if (collision.tag == "Molar" && !hasCollided)
         {
             collision.GetComponent<ToothHealth>().TakeDamage(damage);
+            hasCollided = true;
         }
-        else if (collision.tag == "Canine")
+        else if (collision.tag == "Canine" && !hasCollided)
         {
             collision.GetComponent<ToothHealth>().TakeDamage(damage);
+            hasCollided = true;
         }
-        else if (collision.tag == "Incisor")
+        else if (collision.tag == "Incisor" && !hasCollided)
         {
             collision.GetComponent<ToothHealth>().TakeDamage(damage);
+            hasCollided = true;
         }
     }
 }
--- Assets/Scripts/DENTOID/Enemy/EnemyFollow.cs
+++ Assets/Scripts/DENTOID/Enemy/EnemyFollow.cs
@@ -7,14 +7,16 @@ public class EnemyFollow : MonoBehaviour
     public GameObject specifiedTooth; //Drag desired tooth game object into this script which is attached to an enemy
 
     public float speed; //How fast enemy moves towards tooth, can be changed in inspector
-  
+
 
     private float toothDist;
-   
-    
+
+    [SerializeField] protected float damage;
+
+
     void Update()
     {
-        
+
 
         toothDist = Vector2.Distance(transform.position, specifiedTooth.transform.position); //Distance between enemy and specified tooth
         Vector2 toothDirection = specifiedTooth.transform.position - transform.position; //Difference between the tooth's position and enemy's current position
@@ -22,15 +24,24 @@ public class EnemyFollow : MonoBehaviour
         float toothAngle = Mathf.Atan2(toothDirection.y, toothDirection.x) * Mathf.Rad2Deg;
         //Maths function used to find angle between 2 points   //Converts radians to degrees
 
-            transform.position = Vector2.MoveTowards(this.transform.position, specifiedTooth.transform.position, speed * Time.deltaTime);
-            //Enemy's position          //MoveTowards is a function within Unity that moves something towards a target
-            //"this".transform.position refers to the enemy that the script is attached to, the enemy's position
+        transform.position = Vector2.MoveTowards(this.transform.position, specifiedTooth.transform.position, speed * Time.deltaTime);
+        //Enemy's position          //MoveTowards is a function within Unity that moves something towards a target
+        //"this".transform.position refers to the enemy that the script is attached to, the enemy's position
 
-            transform.rotation = Quaternion.Euler(Vector3.forward * toothAngle);
-            //Quaternion.Euler us just maths stuff concerned with rotations
-            //The .forward is part of Unity, and tells the enemy to go forward
-      
-
+        transform.rotation = Quaternion.Euler(Vector3.forward * toothAngle);
+        //Quaternion.Euler us just maths stuff concerned with rotations
+        //The .forward is part of Unity, and tells the enemy to go forward
 
     }
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == specifiedTooth)
+        {
+            collision.GetComponent<ToothHealth>().TakeDamage(damage);
+           
+        }
+
+    }
+
 }
