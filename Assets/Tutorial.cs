57c9cf6ef40704c1285f671015938bfc4db28a44
Jinnes04 <125297175+Jinnes04@users.noreply.github.com>
2023-03-27 23:29:27 +0100

    TutorialBasicOutline

---
+++ Assets/Tutorial.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialScript : MonoBehaviour
+{
+    [SerializeField] private GameObject TutorialScene;
+    [SerializeField] private GameObject SecondTextBox;
+    [SerializeField] private GameObject KillCounter;
+    [SerializeField] private GameObject Timer;
+    [SerializeField] private Text TutorialText;
+    [SerializeField] private Text TutorialText2;
+    public float TimeValue = 0;
+    float GumGuard = 0;
+
+    void Update()
+    {
+        PopUps();          
+    }
+
+    public void PopUps()
+    {
+        // First pop up starts at 0 seconds      
+        if (TimeValue == 0)
+        {
+            TutorialScene.SetActive(true);
+            SecondTextBox.SetActive(false);
+            TutorialText.text = "Use The Left And Right Arrow Keys To Move";
+            Time.timeScale = 0;
+            GumGuard = 1;
+        }
+        // at 5 seconds it shows the next pop up
+        if (TimeValue > 5 && TimeValue < 5.1)
+        {
+            TutorialScene.SetActive(true);
+            TutorialText.text = "Use The Up Arrow To Jump";
+            Time.timeScale = 0;
+            GumGuard = 1;
+        }
+        // at 7 seconds it shows the next pop up
+        if (TimeValue > 7 && TimeValue < 7.1)
+        {
+            TutorialScene.SetActive(true);
+            TutorialText.text = "Spin Using The Space Bar";
+            Time.timeScale = 0;
+            GumGuard = 1;
+        }
+        // at 10 seconds it shows the next pop up
+        if (TimeValue > 10 && TimeValue < 10.1)
+        {
+            TutorialScene.SetActive(true);
+            SecondTextBox.SetActive(true);
+            TutorialText.text = "Oh No! An Enemy Is Attacking A Tooth Use Your Spin To Kill It";
+            TutorialText2.text = "It Clings On To The Enamal On The Tooth And Releases Acid To Damage It, Use your Spin To Clean The Teeth";
+            Time.timeScale = 0;
+            GumGuard = 1;
+        }
+        // at 15 seconds it shows the next pop up
+        if (TimeValue > 15 && TimeValue < 15.1)
+        {
+            TutorialScene.SetActive(true);
+            SecondTextBox.SetActive(true);
+            TutorialText.text = "If Two Enemys Attach To The Teeth It'll Take Double The Cleaning";
+            TutorialText2.text = "Be Carefull Not To Let Three Enemies Reach A Tooth Otherwise, IT WILL BECOME DECAYED!";
+            Time.timeScale = 0;
+            GumGuard = 1;
+        }
+        // at 20 seconds it shows the next pop up
+        if (TimeValue > 20 && TimeValue < 20.1)
+        {
+            TutorialScene.SetActive(true);
+            SecondTextBox.SetActive(false);
+            Timer.SetActive(true);
+            KillCounter.SetActive(true);
+            TutorialText.text = "You Have 2:00 Minutes To Kill 20 Enemies GET PROTECTING THOSE TEETH!";
+            TutorialText2.text = "Be Carefull Not To Let Three Enemies Reach A Tooth Otherwise, IT WILL BECOME DECAYED!";
+            Time.timeScale = 0;
+            GumGuard = 1;
+        }
+
+        if (GumGuard == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                TutorialScene.SetActive(false);
+                Time.timeScale = 1;
+                GumGuard = 0;
+                TimeValue += 1;
+            }
+
+        }
+
+        TimeValue += Time.deltaTime;       
+    }       
+           
+}
+    
