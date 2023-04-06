c43a9cd13d6871f7cf1eaa8d4d8392c909a19438
JoshuaMaher <jmaher300@caledonian.ac.uk>
2023-04-06 18:28:28 +0100

    changed teeth sprites

--- Assets/Scripts/DENTOID/ToothHealth.cs
+++ Assets/Scripts/DENTOID/ToothHealth.cs
@@ -18,7 +18,7 @@ public class ToothHealth : MonoBehaviour
     [SerializeField] private Sprite health1;  
     [SerializeField] private Sprite health0;  
     private SpriteRenderer spriteRen;
-    public GameObject toothSprite;
+    
     //invunerability
     public float invulTime = 4;
     private int flashNum = 5;
@@ -52,28 +52,24 @@ public class ToothHealth : MonoBehaviour
         {
             //DEFAULT TOOTH SPRITE
             this.gameObject.GetComponent<SpriteRenderer>().sprite = health3;
-            toothSprite.SetActive(false);
             spriteRen.color = Color.white;
         }
         else if(currentHealth == 2)
         {
             //SPRITE WITH PLAQUE ON TOOTH
             this.gameObject.GetComponent<SpriteRenderer>().sprite = health2;
-            toothSprite.SetActive(true);
             spriteRen.color = Color.white;
         }
         else if(currentHealth == 1)
         {
             //SPRITE WITH MORE PLAQUE ON TOOTH
             this.gameObject.GetComponent<SpriteRenderer>().sprite = health1;
-            toothSprite.SetActive(true);
             spriteRen.color = Color.white;
         }
         else if(currentHealth <= 0 && decay)
         {
             //SPRITE WITH DECAYED TOOTH
             this.gameObject.GetComponent<SpriteRenderer>().sprite = health0;
-            toothSprite.SetActive(false);
             spriteRen.color = Color.white;
         }
     }
@@ -121,9 +117,9 @@ public class ToothHealth : MonoBehaviour
         //DURATION OF INVUNERABILITY
         for (int i = 0; i < flashNum; i++)
         {
-            toothSprite.GetComponent<SpriteRenderer>().color = hurtColour; //change colour 
+            this.gameObject.GetComponent<SpriteRenderer>().color = hurtColour; //change colour 
             yield return new WaitForSeconds(invulTime / (flashNum * 2));
-            toothSprite.GetComponent<SpriteRenderer>().color = Color.white;
+            this.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
             yield return new WaitForSeconds(invulTime / (flashNum * 2));
         }
         Physics2D.IgnoreLayerCollision(7, 8, false);
